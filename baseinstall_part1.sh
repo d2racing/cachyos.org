28046198f666057c234cb10b1a33247ff9466adb
Sylvain Alain <d2racing911@gmail.com>
2025-12-04 23:33:30 -0500

    Update baseinstall_part1.sh

--- baseinstall_part1.sh
+++ baseinstall_part1.sh
@@ -6,6 +6,7 @@ echo "=== Début de baseinstall_part1 ==="
 sudo pacman -Sy --noconfirm 
 sudo pacman -S --noconfirm git fastfetch fio btop
 sudo pacman -S --noconfirm yay
-yay -S --noconfirm google-chrome 1password anydesk-bin discord signal-desktop 
+paru -S --noconfirm google-chrome 1password anydesk-bin discord signal-desktop 
 
 echo "=== Installation terminée avec succès ==="
+
