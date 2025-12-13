91eeb11b78c5403abb2ebf4a74ee2d029f98547f
Sylvain Alain <d2racing911@gmail.com>
2025-12-13 09:58:51 -0500

    Update zfs_setup_sdb.sh

--- zfs_setup_sdb.sh
+++ zfs_setup_sdb.sh
@@ -12,11 +12,9 @@ sudo zpool create -f \
 
 sudo zfs create backuppool/nas_backup
 sudo zfs create backuppool/nas_backup/current
-sudo zfs create backuppool/nas_backup/snapshots
 
 sudo zfs set mountpoint=/mnt/backup/nas_backup backuppool/nas_backup
 sudo zfs set mountpoint=/mnt/backup/nas_backup/current backuppool/nas_backup/current
-sudo zfs set mountpoint=/mnt/backup/nas_backup/snapshots backuppool/nas_backup/snapshots
 
 sudo zfs set compression=zstd backuppool
 sudo zfs set atime=off backuppool
