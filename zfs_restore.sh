e505d4aa44e48160934e025813d96d6b0281bea8
Sylvain Alain <d2racing911@gmail.com>
2025-12-13 21:10:44 -0500

    Create zfs_restore.sh

---
+++ zfs_restore.sh
@@ -0,0 +1,14 @@
+zfs list -t snapshot
+
+backuppool/nas/current@snap-2025-12-10_02-00
+backuppool/nas/current@snap-2025-12-11_02-00
+backuppool/nas/current@snap-2025-12-12_02-00
+
+ls /mnt/backup/nas/current/.zfs/snapshot/
+
+SNAP="snap-2025-12-12_02-00"
+time rsync -a /mnt/backup/nas/current/.zfs/snapshot/$SNAP/photo/ /mnt/backup/nas/current/photo/
+
+zfs rollback -r backuppool/nas/current@snap-2025-12-12_02-00
+
+rsync -a /mnt/backup/nas/current/.zfs/snapshot/$SNAP/photo/ /tmp/restore-photo-test/
