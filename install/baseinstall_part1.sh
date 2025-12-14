0bc75341792e58759db9cf765c27a24efeda7232
Sylvain Alain <d2racing911@gmail.com>
2025-12-14 10:08:59 -0500

    Update and rename baseinstall_part1.sh to install/baseinstall_part1.sh

--- install/baseinstall_part1.sh
+++ install/baseinstall_part1.sh
@@ -4,6 +4,5 @@ set -e  # Arrête le script si une commande échoue
 echo "=== Début de baseinstall_part1 ==="
 sudo pacman -Syu  
 sudo pacman -S git fastfetch fio btop
-paru -S google-chrome 1password anydesk-bin discord signal-desktop 
 echo "=== Installation terminée avec succès ==="
 
