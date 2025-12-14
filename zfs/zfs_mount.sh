c4aa5d8d9d24cba058c114c553c7d18397ad19db
Sylvain Alain <d2racing911@gmail.com>
2025-12-14 13:52:43 -0500

    Update zfs_mount.sh

--- zfs/zfs_mount.sh
+++ zfs/zfs_mount.sh
@@ -1,17 +1,4 @@
-sudo systemctl enable zfs-import.target
-sudo systemctl enable zfs-mount.service
-sudo systemctl enable zfs-import-cache.service
-
-sudo systemctl start zfs-import.target
-sudo systemctl start zfs-mount.service
-
+#!/bin/bash
 sudo zpool import
 sudo zpool import backuppool
-
-/mnt/backup/nas_backup
-/mnt/backup/nas_backup/current
-
-sudo zfs mount -a
-
-sudo zfs unmount -R backuppool
-sudo zpool export backuppool
+sudo zfs list 
