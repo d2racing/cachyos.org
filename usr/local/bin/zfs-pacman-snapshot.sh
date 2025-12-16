0ad7a1f8d16fc5b12aa93560377ffbae6abbe642
Sylvain Alain <d2racing911@gmail.com>
2025-12-15 23:25:20 -0500

    Update zfs-pacman-snapshot.sh

--- usr/local/bin/zfs-pacman-snapshot.sh
+++ usr/local/bin/zfs-pacman-snapshot.sh
@@ -5,3 +5,5 @@ SNAP="pre-pacman-$(date +'%Y%m%d-%H%M')"
 
 /usr/bin/zfs snapshot -r "${DATASET}@${SNAP}"
 echo "Snapshot ${DATASET}@${SNAP} created successfully."
+
+# sudo chmod +x /usr/local/bin/zfs-pacman-snapshot.sh
