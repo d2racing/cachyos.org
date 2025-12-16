8c330984f2fe883a2d94f8aec712d995ffd33589
Sylvain Alain <d2racing911@gmail.com>
2025-12-15 23:14:51 -0500

    Update zfs_snapshot_install.sh

--- zfs/zfs_snapshot_install.sh
+++ zfs/zfs_snapshot_install.sh
@@ -7,7 +7,6 @@ DATASET="zpcachyos/ROOT/cos"
 SNAPNAME="snap-$(date +'%Y%m%d-%H:%M')"
 
 echo "Creating ZFS snapshot: ${DATASET}@${SNAPNAME}"
-time sudo zfs snapshot "${DATASET}@${SNAPNAME}"
-
+time sudo zfs snapshot -r zpcachyos/ROOT/cos@snap-$(date +'%Y%m%d-%H%M')
 zfs list -t snapshot
 
