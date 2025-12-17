47cfe6d4e9677d22762b5bd1056419626f6672f3
Sylvain Alain <d2racing911@gmail.com>
2025-12-17 01:08:40 -0500

    Update baseinstall_part2.sh

--- install/baseinstall_part2.sh
+++ install/baseinstall_part2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 set -e  # Arrête le script si une commande échoue
 echo "=== Début de baseinstall_part2 ==="
 paru -S google-chrome 1password anydesk-bin discord signal-desktop 
