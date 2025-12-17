31a6c53629f84a4aa5ce8e7fbc68a0a0f7fd247e
Sylvain Alain <d2racing911@gmail.com>
2025-12-17 11:31:50 -0500

    Update zfs_setup_sdb.sh

--- zfs/backuppool/zfs_setup_sdb.sh
+++ zfs/backuppool/zfs_setup_sdb.sh
@@ -5,16 +5,31 @@ DISK="/dev/sdb"
 wipefs -a $DISK
 sgdisk --zap-all $DISK
 
-zpool create -f \
-  -o ashift=12 \
-  backuppool \
-  $DISK
+# Création du pool
+zpool create -f -o ashift=12 backuppool $DISK
 
+# Création des datasets pour backup normal
 zfs create backuppool/nas_backup
 zfs create backuppool/nas_backup/current
 
 zfs set mountpoint=/mnt/backup/nas_backup backuppool/nas_backup
 zfs set mountpoint=/mnt/backup/nas_backup/current backuppool/nas_backup/current
+
+# Création sécurisée des datasets pour snapshot de système (cachyos_backup)
+zfs create backuppool/cachyos_backup
+zfs create backuppool/cachyos_backup/current
+# Optionnel : si tu veux snapshotter /send
+zfs create backuppool/cachyos_backup/root
+zfs create backuppool/cachyos_backup/home
+zfs create backuppool/cachyos_backup/varcache
+zfs create backuppool/cachyos_backup/varlog
+
+# Sécurisation des datasets système : ne jamais monter automatiquement sur /
+sudo zfs set canmount=noauto backuppool/cachyos_backup/root
+sudo zfs set canmount=noauto backuppool/cachyos_backup/home
+sudo zfs set canmount=noauto backuppool/cachyos_backup/varcache
+sudo zfs set canmount=noauto backuppool/cachyos_backup/varlog
+sudo zfs set mountpoint=none backuppool/cachyos_backup
 
 # Optimisations HDD USB backup
 zfs set compression=zstd backuppool
@@ -30,7 +45,7 @@ zfs set recordsize=1M backuppool
 zfs set recordsize=1M backuppool/nas_backup
 zfs set recordsize=1M backuppool/nas_backup/current
 
+# Vérification finale
 zfs get compression,recordsize,atime,relatime,xattr,redundant_metadata backuppool backuppool/nas_backup/current
 
 echo "WD Elements 14TB prêt pour backup ZFS ✔"
-
