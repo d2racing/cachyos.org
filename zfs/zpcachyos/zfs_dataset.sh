bbe316598d0df34fa095aebb69cea7337d012e08
Sylvain Alain <d2racing911@gmail.com>
2025-12-17 01:23:55 -0500

    Update zfs_dataset.sh

--- zfs/zpcachyos/zfs_dataset.sh
+++ zfs/zpcachyos/zfs_dataset.sh
@@ -1,4 +1,5 @@
-#/bin/bash
+#!/bin/bash
 
 zfs create -o mountpoint=/mnt/cachyos_backup backuppool/cachyos_backup
 zfs create -o mountpoint=/mnt/cachyos_backup/current backuppool/cachyos_backup/current
+
