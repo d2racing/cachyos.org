8fa24eaa2183c3b85f17b8f22a1e526f376940e4
Sylvain Alain <d2racing911@gmail.com>
2025-12-17 01:08:12 -0500

    Update baseinstall_part1.sh

--- install/baseinstall_part1.sh
+++ install/baseinstall_part1.sh
@@ -2,7 +2,6 @@
 set -e  # Arrête le script si une commande échoue
 
 echo "=== Début de baseinstall_part1 ==="
-sudo pacman -Syu  
-sudo pacman -S git fastfetch fio btop
+pacman -Syu  
+pacman -S git fastfetch fio btop
 echo "=== Installation terminée avec succès ==="
-
