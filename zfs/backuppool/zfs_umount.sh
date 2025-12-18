de1daee7dfc007902ed6cdd9b994a5379fc8da9f
Sylvain Alain <d2racing911@gmail.com>
2025-12-17 23:49:19 -0500

    Update zfs_umount.sh

--- zfs/backuppool/zfs_umount.sh
+++ zfs/backuppool/zfs_umount.sh
@@ -4,16 +4,16 @@ set -euo pipefail
 POOL="backuppool"
 DATASET="backuppool/nas_backup/current"
 
-# 1️⃣ Démonte uniquement le dataset utilisé
+# 1⃣ Démonte uniquement le dataset utilisé
 sudo zfs unmount "$DATASET"
 
-# 2️⃣ Vérifie si le dataset est bien démonté
+# 2⃣ Vérifie si le dataset est bien démonté
 mountpoint -q "/mnt/backup/nas_backup/current" && {
     echo "ERROR: $DATASET n'a pas été démonté correctement"
     exit 1
 }
 
-# 3️⃣ Export du pool
+# 3⃣ Export du pool
 sudo zpool export "$POOL"
 
 echo "✅ $POOL exporté avec succès"
