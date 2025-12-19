604711e4a545fac65d60d57f45980224c79cb414
Sylvain Alain <d2racing911@gmail.com>
2025-12-19 17:11:19 -0500

    Update baseinstall_part3.sh

--- install/baseinstall_part3.sh
+++ install/baseinstall_part3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e  # Arrête le script si une commande échoue
-echo "=== Début de baseinstall_part2 ==="
+echo "=== Début de baseinstall_part3 ==="
 paru -S google-chrome 1password anydesk-bin discord signal-desktop 
 echo "=== Installation terminée avec succès ===
 
