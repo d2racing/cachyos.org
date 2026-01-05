e4f97b68cef1447e2ae4ffe60cbdc5c8254257ee
Sylvain Alain <d2racing911@gmail.com>
2026-01-05 00:46:24 -0500

    Move zfs_restore.sh to zpbackup directory

--- zfs/zpbackup/zfs_restore.sh
+++ zfs/zpbackup/zfs_restore.sh
@@ -1,15 +1,15 @@
 zfs list -t snapshot
 
-backuppool/nas/current@snap-2025-12-10_02-00
-backuppool/nas/current@snap-2025-12-11_02-00
-backuppool/nas/current@snap-2025-12-12_02-00
+zpbackup/nas/current@snap-2025-12-10_02-00
+zpbackup/nas/current@snap-2025-12-11_02-00
+zpbackup/nas/current@snap-2025-12-12_02-00
 
 ls /mnt/backup/nas/current/.zfs/snapshot/
 
 SNAP="snap-2025-12-12_02-00"
 time rsync -a /mnt/backup/nas/current/.zfs/snapshot/$SNAP/photo/ /mnt/backup/nas/current/photo/
 
-zfs rollback -r backuppool/nas/current@snap-2025-12-12_02-00
+zfs rollback -r zpbackup/nas/current@snap-2025-12-12_02-00
 
 rsync -a /mnt/backup/nas/current/.zfs/snapshot/$SNAP/photo/ /tmp/restore-photo-test/
 
